4c249c3e9939a164e1e45d48e453fa9cd7283baf
skverma8873 <skverma73@gmail.com>
2025-11-12 08:44:51 +0530

    api main modified skv 3

--- research_and_analyst/api/main.py
+++ research_and_analyst/api/main.py
@@ -12,7 +12,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="research_and_analyst/api/templates")
 app.templates = templates  # so templates accessible inside router
 # skv 
-# skv 2
+# skv 3
 # 🔹 ADD THIS FUNCTION
 def basename_filter(path: str):
     return os.path.basename(path)
